27b05783dcb8f165649871df0a509c321ef859d0
Miguel Uranga <57147319+Lemonaeds@users.noreply.github.com>
2024-10-21 18:37:19 +0000

    Cambios para mejores practicas

--- src/app.js
+++ src/app.js
@@ -6,41 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  //declaracion de cada uno de los arreglos a explorar
   let pronoun = ["the", "our", "my"];
-  let adj = ["great", "big", "tiny", "smidgen", "smallest", "greatest"];
+  let adjective = ["great", "big", "tiny", "smidgen", "smallest", "greatest"];
   let noun = ["jogger", "racoon", "human", "cat", "dog", "game", "stadium"];
   let extension = [".com", ".co", ".gov", ".us", ".to", ".net"];
-
-  console.log("Here are all the combinations!");
+  let urlName = "";
   //exploracion de cada uno de los arrays para poder crear el nombre de dominio
   //exploracion del titulo por pronombre
   for (let pronombre = 0; pronombre < pronoun.length; pronombre++) {
     //exploracion del titulo por adjetivo
-    for (let adjetivo = 0; adjetivo < adj.length; adjetivo++) {
-      console.log(pronoun[pronombre] + "." + adj[adjetivo]);
+    urlName = pronoun[pronombre];
+    extensionAddition(urlName, extension);
+
+    for (let adjetivo = 0; adjetivo < adjective.length; adjetivo++) {
       //exploracion del titulo por sustantivo
+      urlName = pronoun[pronombre] + adjective[adjetivo];
+      extensionAddition(urlName, extension);
+
       for (let sustantivo = 0; sustantivo < noun.length; sustantivo++) {
-        console.log(
-          pronoun[pronombre] + adj[adjetivo] + "." + noun[sustantivo]
-        );
         //exploracion del titulo por extension
-        for (let ext = 0; ext < extension.length; ext++) {
-          console.log(
-            pronoun[pronombre] +
-              adj[adjetivo] +
-              noun[sustantivo] +
-              extension[ext]
-          );
-          document.getElementById("url").innerText +=
-            pronoun[pronombre] +
-            adj[adjetivo] +
-            noun[sustantivo] +
-            extension[ext] +
-            "\n";
-        }
+        urlName = pronoun[pronombre] + adjective[adjetivo] + noun[sustantivo];
+        extensionAddition(urlName, extension);
       }
     }
   }
   //fin del for
 };
+
+//Funcion para añadir la extension a cada una de las posibles combinaciones
+function extensionAddition(urlName, extension) {
+  for (let ext = 0; ext < extension.length; ext++) {
+    document.getElementById("url").innerText += urlName + extension[ext] + "\n";
+  }
+}
